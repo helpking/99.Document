dbc332146685cff76ee3148663ea4606f4597968
何 利強 <ka_rikyou@applibot.co.jp>
2015-11-25 15:44:15 +0900

    Nodejsのアンインストールシェルの見直しとPomoleのインストールメモの変更

--- 98.ShellTool/100.UninstallNodeJs.sh
+++ 98.ShellTool/100.UninstallNodeJs.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-lsbom -f -l -s -pf /var/db/receipts/org.nodejs.pkg.bom \
-| while read i; do
-  sudo rm /usr/local/${i}
-done
-sudo rm -rf /usr/local/lib/node \
-     /usr/local/lib/node_modules \
-     /var/db/receipts/org.nodejs.*
+if [ -f /var/db/receipts/org.nodejs.pkg.bom ]; then
+  
+  lsbom -f -l -s -pf /var/db/receipts/org.nodejs.pkg.bom \
+  | while read i; do
+    sudo rm /usr/local/${i}
+  done
+  sudo rm -rf /usr/local/lib/node \
+       /usr/local/lib/node_modules \
+       /var/db/receipts/org.nodejs.*
+ 
+else
+  
+  lsbom -f -l -s -pf /var/db/receipts/org.nodejs.node.pkg.bom \
+  | while read i; do
+    sudo rm /usr/local/${i}
+  done
+  sudo rm -rf /usr/local/lib/node \
+       /usr/local/lib/node_modules \
+       /var/db/receipts/org.nodejs.*
+
+fi
+
+exit;
+
 
---
+++ 99.GameSample/099.Server/ReadMe.txt
@@ -0,0 +1,36 @@
+Server 配置手順メモ
+
+1) Node.jsのインストール
+　　ダウンロード(v4.2.2/v4.2.1)URL:https://nodejs.org/en/
+    注意：node.jsとnode-gypのバージョンが違う場合、インストール失敗の可能性があります。
+
+　　インストール成功のチェック
+
+　　Node was installed at
+
+   　　/usr/local/bin/node
+
+　　npm was installed at
+
+   　　/usr/local/bin/npm
+
+　　Make sure that /usr/local/bin is in your $PATH.
+
+  バージョンチェックのコマンド
+　node --version
+
+2) python(2.5 < version < 3.0)
+
+　バージョンチェックのコマンド
+　python --version
+
+3) C++コンパイラ
+　Xcode Command Line Tools / XCode 
+  XCode Command Line Tools インストール用のURL
+  https://developer.apple.com/downloads/
+
+
+
+PS:
+ 1) 参照資料
+　　Pomeloマニアル：https://github.com/cynron/pomelo/wiki/中文文档首页
