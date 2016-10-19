303908eb3e83c098a7ee9bbaa34f015df043a26a
何 利強 <ka_rikyou@applibot.co.jp>
2016-10-19 22:46:15 +0900

    @@ CMakeの勉強メモ（006）：Qtツールキット @@

---
+++ 00.Document/01.CMake/006/prog7.cc
@@ -0,0 +1,15 @@
+// Begin "prog7.cc"
+// C++ Program 7
+#include <QApplication>
+#include <QTextEdit>
+
+int main(int argv, char **args)
+{
+  QApplication app(argv, args);
+
+  QTextEdit textEdit;
+  textEdit.show();
+  return app.exec();
+}
+// End "prog7.cc"
+
