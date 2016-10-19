c445fa5db8528d7ec362aa3220e38185b3947fed
何 利強 <ka_rikyou@applibot.co.jp>
2016-10-19 14:14:16 +0900

    @@ CMakeの勉強メモ（001） ・複数ファイルコンパイル
    @@

---
+++ 00.Document/01.CMake/001/prog2.cc
@@ -0,0 +1,15 @@
+
+// Begin "prog2.cc"
+// C++ Program 2
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << "This is prog2.\n";
+  return 0;
+}
+// End "prog2.cc"
+
