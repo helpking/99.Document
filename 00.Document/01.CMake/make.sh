0a2eff2763c40f75a4028f2944491cabdec97c98
何 利強 <ka_rikyou@applibot.co.jp>
2016-10-21 18:26:30 +0900

    @@@ CMakeの勉強メモ（005） @@@

--- 00.Document/01.CMake/make.sh
+++ 00.Document/01.CMake/make.sh
@@ -1,11 +1,23 @@
 #!/bin/bash 
 
+# カレントディレクトリー
 CUR_DIR=`pwd`
+# 該当ディレクトリー
 WORK_DIR=""
+# CMake
 CMAKE_PREFIX_PATH=""
+# クリアフラグ（コンパイル後で、ソース、かつ、バイナリーターゲットファイル以外、全部削除）
 CLEANUP=0
+# テストフラグ（コンパイル後で、ソース以外、全部削除）
 IS_TEST=0
-BIN_DIR="./bin"
+# ターゲットバイナリーディレクトリー
+TARGET_BIN_DIR="./bin"
+#CMakeディレクトリー
+CMAKE_DIR=""
+#CMakelistsファイル
+CUR_CMAKE_LISTS_FILE_NAME="CMakeLists.txt"
+CMAKE_LISTS_FILE="${CMAKE_DIR}/${CUR_CMAKE_LISTS_FILE_NAME}"
+CUR_CMAKE_LISTS_FILE_PATH="./${CUR_CMAKE_LISTS_FILE}"
 
 if [[ $# -lt 1 ]];then
     echo "Command Format:`basename $0` -d dir [-m cmake prefix path] [-c cleanup] [-t test flag]"
@@ -16,6 +28,9 @@ while getopts d:m:ct opt; do
 case $opt in   
 	d)  
 		WORK_DIR=$OPTARG
+        CMAKE_DIR="${WORK_DIR}/cmake"
+        CMAKE_LISTS_FILE="${CMAKE_DIR}/${CUR_CMAKE_LISTS_FILE_NAME}" 
+        CUR_CMAKE_LISTS_FILE_PATH="${WORK_DIR}/${CUR_CMAKE_LISTS_FILE_NAME}"
 		;; 
     m)
         CMAKE_PREFIX_PATH=$OPTARG
@@ -31,13 +46,27 @@ case $opt in
 esac  
 done  
 
-if [ ! -d "$WORK_DIR" ]; then 
+if [ ! -d "${WORK_DIR}" ]; then 
 	echo "the direct is not exist!![Dir=$WORK_DIR]"
 	exit 2
 fi
 
+if [ ! -d "${CMAKE_DIR}" ]; then 
+	echo "the direct is not exist!![Dir=$CMAKE_DIR]"
+	exit 2
+fi
+
+if [ ! -f "${CMAKE_LISTS_FILE}" ]; then 
+	echo "the file is not exist!![file=$CMAKE_LISTS_FILE]"
+	exit 3
+fi
+#
+cp -fp ${CMAKE_LISTS_FILE} ${CUR_CMAKE_LISTS_FILE_PATH}
+
 cd ${WORK_DIR}
-mkdir ${BIN_DIR}
+if [ ! -d "${TARGET_BIN_DIR}" ]; then 
+  mkdir ${TARGET_BIN_DIR}
+fi
 
 if [ -f "./make.sh" ]; then
   sh ./make.sh
@@ -54,7 +83,7 @@ make
 
 # Clean
 if [ "${IS_TEST}" == "1" ]; then
-  rm -rf ${BIN_DIR}
+  rm -rf ${TARGET_BIN_DIR}
   rm -f ./CMakeCache.txt
   rm -rf ./CMakeFiles/
   rm -f ./Makefile
@@ -69,3 +98,8 @@ else
 fi
 
 cd $CUR_DIR
+
+# CMakelistsファイルを削除する
+if [ -f "${CUR_CMAKE_LISTS_FILE_PATH}" ]; then 
+  rm -f ${CUR_CMAKE_LISTS_FILE_PATH}
+fi
