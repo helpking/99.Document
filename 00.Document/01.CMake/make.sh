17a334f215d40fcdfb3381974be37cde3d0198bc
何 利強 <ka_rikyou@applibot.co.jp>
2016-10-21 10:40:22 +0900

    @@@ コンパイル用のmake.shファイルの作成 @@@

--- 00.Document/01.CMake/make.sh
+++ 00.Document/01.CMake/make.sh
@@ -3,51 +3,62 @@
 CUR_DIR=`pwd`
 DIR=""
 CMAKE_PREFIX_PATH=""
-CLEANUP=1
+CLEANUP=0
+IS_TEST=0
 
 if [[ $# -lt 1 ]];then
-    echo "Command Format:`basename $0` -d dir [-m cmake prefix path] [-c cleanup]"
+    echo "Command Format:`basename $0` -d dir [-m cmake prefix path] [-c cleanup] [-t test flag]"
     exit 1
 fi
 
-while getopts d:m:c: opt; do  
+while getopts d:m:ct opt; do  
 case $opt in   
 	d)  
 		DIR=$OPTARG
 		;; 
-        m)
-                CMAKE_PREFIX_PATH=$OPTARG
-                ;;
-        c)
-                CLEANUP=$OPTARG
-                ;;
+    m)
+        CMAKE_PREFIX_PATH=$OPTARG
+        ;;
+    c)
+        CLEANUP=1
+        ;;
+    t)
+        IS_TEST=1
+        ;;
 	*)  
 		;;  
 esac  
 done  
 
-if [ ! -d $DIR ]; then 
+if [ ! -d "$DIR" ]; then 
 	echo "the direct is not exist!![Dir=$DIR]"
 	exit 2
 fi
 
 cd $DIR
 
-if [ ${CMAKE_PREFIX_PATH} == "" ]; then
-  cmake .
+if [ -f "./make.sh" ]; then
+  sh ./make.sh
 else
-  cmake . -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+  if [ -z "${CMAKE_PREFIX_PATH}" ]; then
+    cmake .
+  else
+    cmake . -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+  fi
 fi
 
 #Complie
 make
 
 # Clean
-if [ ${CLEANUP} == "1" ]; then
-  rm -f ./CMakeCache.txt
-  rm -rf ./CMakeFiles/
-  rm -f ./Makefile
-  rm -f ./cmake_install.cmake
+if [ "${IS_TEST}" == "1" ]; then
+#else
+  if [ "${CLEANUP}" == "1" ]; then
+    rm -f ./CMakeCache.txt
+    rm -rf ./CMakeFiles/
+    rm -f ./Makefile
+    rm -f ./cmake_install.cmake
+  fi
 fi
 
 cd $CUR_DIR
