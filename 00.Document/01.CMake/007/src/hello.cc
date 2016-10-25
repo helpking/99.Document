ce06a0cf782c9aaf01d1ff2844910f1c9fdcd12b
何 利強 <ka_rikyou@applibot.co.jp>
2016-10-25 13:02:30 +0900

    @@@ CMakeの勉強メモ（007） @@@

--- 00.Document/01.CMake/007/src/hello.cc
+++ 00.Document/01.CMake/007/src/hello.cc
@@ -4,11 +4,10 @@
 
 int main(int argc, char **argv)
 {
-    //QApplication app(argc, argv);
-    //QLabel *label = new QLabel("Hello Qt!");
-    //label->show();
-    //return app.exec();
-    return 0;
+    QApplication app(argc, argv);
+    QLabel *label = new QLabel("Hello Qt!");
+    label->show();
+    return app.exec();
 }
 // End "hello.cc"
 
